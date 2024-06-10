90d0566c319e062860f8db9df18efeeebbe62a8c
DavidDai1997 <113506848+DavidDai1997@users.noreply.github.com>
2024-06-10 16:42:39 +0800

    0610 t2

--- sketch test.js
+++ sketch test.js
@@ -58,7 +58,7 @@ function draw() {
     if (!soundLoadedFlag) {
         text('Loading sound...', width / 2, height / 2);
     } else if (trialPhase === -1) {
-        text('Welcome! Press the spacebar to start.', width / 2, height / 2);
+        text('Welcome! Press the spacebar to start. google', width / 2, height / 2);
         if (keyPressOccurred) {
             userStartAudio(); // Resume the AudioContext
             keyPressOccurred = false; // Reset key press flag
